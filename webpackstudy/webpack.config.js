4eebed6985eee65e131dc18b107f868677cd7d17
Your Name <you@example.com>
2019-03-26 22:15:47 +0800

    html-webpack-plugin

--- webpackstudy/webpack.config.js
+++ webpackstudy/webpack.config.js
@@ -1,12 +1,30 @@
 let path = require("path");
-
+let HtmlWebpackPlugin = require('html-webpack-plugin')
 
 
 module.exports = {
-    mode : 'development', //默认2种模式 production-生产 development-开发
+    devServer : {
+        port : 3000,
+        progress : true,
+        contentBase : './dist',
+        compress : true
+    },
+    mode : 'production', //默认2种模式 production-生产 development-开发
     entry : './src/index.js', //入口
     output : {
-        filename : "bundle.js", //打包后文件名
+        filename : "bundle.[hash:8].js", //打包后文件名
         path : path.resolve(__dirname,'dist'),// 必须是绝对路径
-    }
+    },
+    plugins :[ // 数组 存放所有webpack插件
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            minify:{
+                removeAttributeQuotes: true,
+                collapseWhitespace: true,
+               
+            },
+            hash: true
+        })
+    ]
 }
