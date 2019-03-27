be7eb4c5cd0498184fef0e2f981a6c4b61111e5b
saber <441875572@qq.com>
2019-03-27 11:23:44 +0800

    css less 打包

--- webpackstudy/webpack.config.js
+++ webpackstudy/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
         contentBase : './dist',
         compress : true
     },
-    mode : 'production', //默认2种模式 production-生产 development-开发
+    mode : 'development', //默认2种模式 production-生产 development-开发
     entry : './src/index.js', //入口
     output : {
         filename : "bundle.[hash:8].js", //打包后文件名
@@ -26,5 +26,37 @@ module.exports = {
             },
             hash: true
         })
-    ]
+    ],
+    module: { // 模块
+        // loader
+        rules: [ //规则
+            // style-loader 把CSS插入到head中
+            // css-loader 解析处理 @import 语法
+            { 
+                test: /\.css$/, 
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            insertAt: 'top'
+                        }
+                    },
+                     'css-loader'
+                    ]
+            },
+            { 
+                test: /\.less$/, 
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            insertAt: 'top'
+                        }
+                    },
+                     'css-loader',  // import 解析路径
+                     'less-loader' // 把less -> css
+                    ]
+            },
+        ]
+    }
 }
