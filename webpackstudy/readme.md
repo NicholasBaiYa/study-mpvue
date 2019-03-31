4d49dd7e7d026be906da384c0e993f5e09ed942b
Your Name <you@example.com>
2019-03-31 22:52:24 +0800

    样式抽离

---
+++ webpackstudy/readme.md
@@ -0,0 +1,5 @@
+ ###1. 本地安装 webpack webpack-cli
+ ###2. 安装本地服务 webpack-dev-server
+ ###3. 安装htmlwebpack插件 html-webpack-plugin
+ ###4. 安装style-loader css-loader less less-looader 处理样式
+ ###5. 安装mini-css-extract-plugin 抽离样式
--- webpackstudy/webpack.config.js
+++ webpackstudy/webpack.config.js
@@ -1,9 +1,10 @@
 let path = require("path");
-let HtmlWebpackPlugin = require('html-webpack-plugin')
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 
 module.exports = {
-    devServer : {
+    devServer : { // 开发服务器的配置
         port : 3000,
         progress : true,
         contentBase : './dist',
@@ -15,16 +16,18 @@ module.exports = {
         filename : "bundle.[hash:8].js", //打包后文件名
         path : path.resolve(__dirname,'dist'),// 必须是绝对路径
     },
-    plugins :[ // 数组 存放所有webpack插件
+    plugins :[ // 数组格式 存放所有webpack插件
         new HtmlWebpackPlugin({
             template: './src/index.html',
             filename: 'index.html',
             minify:{
                 removeAttributeQuotes: true,
-                collapseWhitespace: true,
-               
+                collapseWhitespace: true,              
             },
             hash: true
+        }),
+        new MiniCssExtractPlugin({
+            filename: "main.css"
         })
     ],
     module: { // 模块
@@ -32,18 +35,22 @@ module.exports = {
         rules: [ //规则
             // style-loader 把CSS插入到head中
             // css-loader 解析处理 @import 语法
+            // loader的顺序从右向左执行 从下向上执行
             { 
                 test: /\.css$/, 
                 use: [
-                    {
-                        loader: 'style-loader',
-                        options: {
-                            insertAt: 'top'
-                        }
-                    },
+                    // {
+                    //     loader: 'style-loader',
+                    //     options: {
+                    //         insertAt: 'top'
+                    //     }
+                    // },
+                    MiniCssExtractPlugin.loader,
                      'css-loader'
                     ]
             },
+
+            // 可以处理less sass 等
             { 
                 test: /\.less$/, 
                 use: [
